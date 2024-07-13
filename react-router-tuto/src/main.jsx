4a5b1340861c89957dbdc78ce67923d6f186a3b4
AndrewSecond <31904920+AndrewSecond@users.noreply.github.com>
2024-07-14 00:09:35 +0300

    Update main.jsx

--- react-router-tuto/src/main.jsx
+++ react-router-tuto/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Root from './routes/root';
+import './index.css';
+
+const router = createBrowserRouter([
+  // This first route is what we often call the "root route" 
+  // since the rest of our routes will render inside of it. 
+  // It will serve as the root layout of the UI, we'll have  
+  // nested layouts as we get farther along.
+  {
+    path: "/",
+    element: <Root />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
