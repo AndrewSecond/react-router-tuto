b7ca4f37412d4e07014049a9b44b2a2f6c9c24ae
Master <belyand@mail.ru>
2024-07-25 13:36:38 +0300

    true

--- react-router-tuto/src/main.jsx
+++ react-router-tuto/src/main.jsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
       {
         index: true, 
         element: <Index />,
-      }      
+      },      
       {
         path: "contacts/:contactId",
         element: <Contact />,
